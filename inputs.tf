8920f923056f23003ba208350c87fc324d7d0a97
Cory Forsythe <cory@forsythenetworks.com>
2022-03-07 18:41:00 -0600

    types should not be quoted

--- inputs.tf
+++ inputs.tf
@@ -1,24 +1,29 @@
 variable "VPC_Name" {
-    type = "string"
+    type = string
     default = "TF-VPC"
 }
 
 variable "VPC_Cidr" {
-    type="string"
+    type=string
     default = "10.0.0.0/8"
 }
 
 variable "VPC_Private_Subnets" {
-    type="list(string)"
+    type=list(string)
     default=["10.1.0.0/16","10.100.0.0/16"]
 }
 
 variable "VPC_Public_Subnets" {
-    type="list(string)"
+    type=list(string)
     default=["10.1.0.0/16","10.100.0.0/16"]
 }
 
 variable "AWS_Region" {
-  type="string"
+  type=string
   default="us-east-2"
+}
+
+variable "Environment" {
+    type = string
+    default = "test"
 }
