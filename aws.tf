f3480470b5bfc0bed95da202bae14f3540c58028
cory.forsythe <cory.forsythe@servicenow.com>
2022-03-09 12:08:33 -0600

    refactor provider

--- aws.tf
+++ aws.tf
@@ -1,6 +1,8 @@
-# Configure the AWS Provider
-provider "aws" {
-}
+1 provider "aws" {
+2   access_key          = var.access_key
+3   secret_key          = var.secret_key
+4   region              = var.aws_region
+6 }
 
 data "aws_availability_zones" "available" {
   state = "available"
--- inputs.tf
+++ inputs.tf
@@ -1,3 +1,9 @@
+variable "access_key" {
+2 }
+
+3 variable "secret_key" {
+4 }
+
 variable "VPC_Name" {
 
 }
@@ -8,13 +14,23 @@ variable "VPC_Cidr" {
 }
 
 variable "VPC_Private_Subnets" {
-
+ type=list(string)
+ default=["10.0.1.0/24","10.0.101.0/24"]
 }
 
 variable "VPC_Public_Subnets" {
+     type=list(string)
+ default=["10.0.0.0/24","10.0.100.0/24"]
 }
 
 
 variable "Environment" {
-
+type="string"
+default="nonrpod
 }
+
+20 variable "aws_region" {
+21 	description = "AWS region to launch servers."
+22   	default     = "us-east-1"
+23 }
+24 
