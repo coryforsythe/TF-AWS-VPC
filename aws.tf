95364da72d37c36e6dd60c346d594eb15bc3b473
Cory Forsythe <cory@forsythenetworks.com>
2022-03-09 12:31:50 -0600

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,8 +1,8 @@
-1 provider "aws" {
-2   access_key          = var.access_key
-3   secret_key          = var.secret_key
-4   region              = var.aws_region
-6 }
+ provider "aws" {
+   access_key          = var.access_key
+   secret_key          = var.secret_key
+   region              = var.aws_region
+}
 
 data "aws_availability_zones" "available" {
   state = "available"
